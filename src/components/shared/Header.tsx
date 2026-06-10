79ffb04d73e9298f61c32a4ce53ca918179bb205
Ramos <ra.920710@gmail.com>
2026-06-10 21:09:21 +0700

    style: keep Lottie eye centered and large inside w-12 cropped wrapper to resolve spacing issues

--- src/components/shared/Header.tsx
+++ src/components/shared/Header.tsx
@@ -104,31 +104,33 @@ const Header: React.FC = () => {
       <div className="flex items-center gap-4">
         <Link
           href="/admin/login"
-          className="flex h-8 w-20 shrink-0 cursor-pointer items-center justify-center overflow-hidden"
+          className="relative flex h-8 w-12 shrink-0 cursor-pointer items-center justify-center overflow-hidden"
           aria-label="Admin Login"
         >
-          {logoAnimationData ? (
-            <Lottie
-              lottieRef={lottieRef}
-              animationData={logoAnimationData}
-              loop={!prefersReducedMotion}
-              autoplay={!prefersReducedMotion}
-              rendererSettings={{ preserveAspectRatio: 'xMidYMid meet' }}
-              style={{ width: 112, height: 112 }}
-              aria-hidden="true"
-            />
-          ) : (
-            <svg
-              width="15"
-              height="18"
-              viewBox="0 0 17 20"
-              fill="black"
-              xmlns="http://www.w3.org/2000/svg"
-              aria-hidden="true"
-            >
-              <path d="M11.6661 17.6533C10.7495 18.9959 9.68947 19.9572 8.52947 20C7.61613 20 7.18947 19.6826 6.32947 19.6826C5.4628 19.6826 4.9628 19.6826 4.09613 20C3.0028 19.9714 2.05613 18.9959 1.15613 17.1666C-0.650534 13.9166 -0.563868 8.64731 2.76947 6.84865C3.8428 6.27398 4.71613 6.13131 5.5628 6.13131C6.55613 6.13131 7.22947 6.74465 8.16947 6.74465C9.09613 6.74465 9.77613 5.96598 10.9561 6.13131C11.5161 6.17398 13.0695 6.36065 14.1228 7.89398C14.0761 7.94731 12.0361 9.13131 12.0761 11.5313C12.1161 14.3473 14.5428 15.3087 14.5961 15.3487C14.5828 15.394 14.2295 16.642 13.5628 17.6133L11.6661 17.6533ZM11.1361 4.10065C11.5961 3.52598 11.9161 2.75931 11.8228 1.95665C11.0828 2.02865 10.1961 2.45798 9.66947 3.09798C9.17613 3.65798 8.7628 4.45798 8.87613 5.23131C9.69613 5.29531 10.5561 4.79398 11.1361 4.10065Z" />
-            </svg>
-          )}
+          <div className="pointer-events-none absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
+            {logoAnimationData ? (
+              <Lottie
+                lottieRef={lottieRef}
+                animationData={logoAnimationData}
+                loop={!prefersReducedMotion}
+                autoplay={!prefersReducedMotion}
+                rendererSettings={{ preserveAspectRatio: 'xMidYMid meet' }}
+                style={{ width: 112, height: 112 }}
+                aria-hidden="true"
+              />
+            ) : (
+              <svg
+                width="15"
+                height="18"
+                viewBox="0 0 17 20"
+                fill="black"
+                xmlns="http://www.w3.org/2000/svg"
+                aria-hidden="true"
+              >
+                <path d="M11.6661 17.6533C10.7495 18.9959 9.68947 19.9572 8.52947 20C7.61613 20 7.18947 19.6826 6.32947 19.6826C5.4628 19.6826 4.9628 19.6826 4.09613 20C3.0028 19.9714 2.05613 18.9959 1.15613 17.1666C-0.650534 13.9166 -0.563868 8.64731 2.76947 6.84865C3.8428 6.27398 4.71613 6.13131 5.5628 6.13131C6.55613 6.13131 7.22947 6.74465 8.16947 6.74465C9.09613 6.74465 9.77613 5.96598 10.9561 6.13131C11.5161 6.17398 13.0695 6.36065 14.1228 7.89398C14.0761 7.94731 12.0361 9.13131 12.0761 11.5313C12.1161 14.3473 14.5428 15.3087 14.5961 15.3487C14.5828 15.394 14.2295 16.642 13.5628 17.6133L11.6661 17.6533ZM11.1361 4.10065C11.5961 3.52598 11.9161 2.75931 11.8228 1.95665C11.0828 2.02865 10.1961 2.45798 9.66947 3.09798C9.17613 3.65798 8.7628 4.45798 8.87613 5.23131C9.69613 5.29531 10.5561 4.79398 11.1361 4.10065Z" />
+              </svg>
+            )}
+          </div>
         </Link>
         <div className="hidden cursor-default px-2 py-1 font-bold sm:block">{appName}</div>
 
