2e4edb0e86aa8c71685812db8c50d4e303f9149b
Ramos <ra.920710@gmail.com>
2026-08-13 00:24:00 +0700

    chore(cleanup): purge legacy landing page and complex project form files

--- src/components/projects/project-detail/components/index.ts
+++
@@ -1,9 +0,0 @@
-export { ProjectBackButton } from './ProjectBackButton';
-export { ProjectCover } from './ProjectCover';
-export { ProjectHeader } from './ProjectHeader';
-export { ProjectMeta } from './ProjectMeta';
-export { ProjectNarrative } from './ProjectNarrative';
-export { ProjectGallery } from './ProjectGallery';
-export { ProjectInteractionBar } from './ProjectInteractionBar';
-export { ProjectRelated, ProjectRelatedColumn, useInfiniteProjects } from './ProjectRelated';
-export { ProjectComments } from './ProjectComments';
--- src/components/projects/project-detail/hooks/index.ts
+++
@@ -1,1 +0,0 @@
-export { useProjectDetail } from './useProjectDetail';
